01c0c365e614ee814d28c23b5dd7f6eb2f6dc68c
chen-yeyu <chen.yeyu@connecty.co.jp>
2017-09-29 13:55:04 +0900

    login.clj 更新不要コードをコメントアウトする

--- src/def/handler/login.clj
+++ src/def/handler/login.clj
@@ -8,15 +8,16 @@
 
 
 (defmethod ig/init-key :def.handler/login [_ options]
-  (context "/login" []
-           (GET "/" [] (io/resource "def/handler/example/login.html"))
-           (POST "/post-login" [username password]
-                 (str "user: " username " password: " password))
-           (GET "/selmer-test1" []
-                (render "Hello {{name}}!" {:name "test-selmer1"}))
-           (GET "/selmer-test2" []
-                (render-file "def/handler/example/userInfo.html"
-                             {:title "test-selmer2" :name "CYY"})))
+  ;
+  ; (context "/login" []
+  ;          (GET "/" [] (io/resource "def/handler/example/login.html"))
+  ;          (POST "/post-login" [username password]
+  ;                (str "user: " username " password: " password))
+  ;          (GET "/selmer-test1" []
+  ;               (render "Hello {{name}}!" {:name "test-selmer1"}))
+  ;          (GET "/selmer-test2" []
+  ;               (render-file "def/handler/example/userInfo.html"
+  ;                            {:title "test-selmer2" :name "CYY"})))
 
   (context "/account" []
            (GET "/" [] (io/resource "def/handler/example/account.html"))
